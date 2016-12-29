b0d0f65bc8bc8ff50715fc2817c0400e813b124a
Dieter Komendera <dieter@komendera.com>
2016-12-29 16:58:17 +0100

    Set helm-M-x-fuzzy-match and clean up

--- init.el
+++ init.el
@@ -41,28 +41,6 @@
 
 (defvaralias 'c-basic-offset 'tab-with)
 
-(defun my-setup-indent (n)
-;; java/c/c++
-(setq c-basic-offset n)
-;; web development
-(setq coffee-tab-width n) ; coffeescript
-(setq javascript-indent-level n) ; javascript-mode
-(setq js-indent-level n) ; js-mode
-(setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
-(setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
-(setq web-mode-css-indent-offset n) ; web-mode, css in html file
-(setq web-mode-code-indent-offset n) ; web-mode, js code in html file
-(setq css-indent-offset n) ; css-mode
-(setq-default tab-width 2)
-;; adjust indents for web-mode to 2 spaces
-(defun my-web-mode-hook ()
- "Hooks for Web mode. Adjust indents"
-   ;;; http://web-mode.org/
-   (my-setup-indent 2))
-(add-hook 'web-mode-hook  'my-web-mode-hook)
-
-(my-setup-indent 2)
-
 (use-package parinfer
   :ensure t
   :bind
@@ -76,8 +54,8 @@
             smart-yank))   ; Yank behavior depend on mode.
     (add-hook 'clojure-mode-hook #'parinfer-mode)
     
-    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode))
-  (setq parinfer-auto-switch-indent-mode nil)))
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+   (setq parinfer-auto-switch-indent-mode nil)))
 
 (use-package smartparens-config
   :ensure smartparens
@@ -102,6 +80,7 @@
 (projectile-global-mode)
 
 (require 'helm-projectile)
+(setq helm-M-x-fuzzy-match 1)
 
 ;; asks for file to open when project is switched
 (setq projectile-switch-project-action 'helm-projectile-find-file)
