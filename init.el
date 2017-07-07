1273138b2bb1905125dc111d2fe17064e4f869d4
Martin Kavalar <martin@sauspiel.de>
2017-07-07 11:19:32 +0200

    Use paredit and rainbow-delimiters

--- init.el
+++ init.el
@@ -7,7 +7,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters use-package spinner smartparens sass-mode rainbow-mode queue parinfer paredit pallet neotree material-theme markdown-mode+ magit inf-clojure guide-key exec-path-from-shell dumb-jump dracula-theme counsel-projectile counsel-osx-app clojure-mode-extra-font-locking alchemist ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
